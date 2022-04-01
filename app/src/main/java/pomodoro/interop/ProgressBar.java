9ad736658a6e341b1dbd06ed8ac1617753e3b51d
Joseph Diza <josephm.diza@gmail.com>
2022-04-01 15:10:28 -0700

    Call libpomodoro timer functions natively in Java

---
+++ app/src/main/java/pomodoro/interop/ProgressBar.java
@@ -0,0 +1,17 @@
+package pomodoro;
+
+import com.sun.jna.*;
+//@FieldOrder({"maxlength", "curlength", "fillchar", "emptychar", "leftmargin", "rightmargin", "cur", "end"})
+//public static class ProgressBar extends Structure {
+    //NativeLong maxlength;
+    //NativeLong curlength;
+
+    //byte fillchar;
+    //byte emptychar;
+    //byte leftmargin;
+    //byte rightmargin;
+
+    //long cur;
+    //long end;
+
+//}
--- app/src/main/java/pomodoro/interop/Timer.java
+++ app/src/main/java/pomodoro/interop/Timer.java
@@ -1,8 +1,12 @@
 package pomodoro;
 
-public class Timer {
-  public int shortbreak;
-  public int longbreak;
-  public int interval_count;
-  public int[] workintervals;
-}
+import com.sun.jna.*;
+
+//@FieldOrder({ "shortbreak", "longbreak", "interval_count", "pb", "workintervals" })
+//public static class Timer extends Structure {
+  //public int shortbreak;
+  //public int longbreak;
+  //public int interval_count;
+  //ProgressBar pb;
+  //public int[] workintervals;
+//}
