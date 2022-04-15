8722f96aca4d7bdd18aea45bca494caa333ea0f2
Joseph Diza <josephm.diza@gmail.com>
2022-04-14 21:08:04 -0700

    Use JUnit 5 Jupiter unit testing framework

--- app/src/test/java/com/github/jmdaemon/pomodoro/ExampleUnitTest.java
+++ app/src/test/java/com/github/jmdaemon/pomodoro/ExampleUnitTest.java
@@ -1,8 +1,11 @@
 package com.github.jmdaemon.pomodoro;
 
-import org.junit.Test;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*; 
+import org.junit.jupiter.api.*;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
